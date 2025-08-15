52a21838474323f543c3c8fe55a22f205b11511a
nuthansamarth <2024cs_nuthansamarthah_c@nie.ac.in>
2025-08-15 12:01:10 +0530

    even and tables

---
+++ even.py
@@ -0,0 +1,7 @@
+start_range=int(input("Enter the start range: "))
+end_range=int(input("Enter the end range: "))
+l1=[]
+for i in range(start_range, end_range + 1):
+    if i % 2 == 0:
+        l1.append(i)
+print(l1)
